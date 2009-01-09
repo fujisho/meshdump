7642abf4ee8cfe8d5d821e77113d3ebc81033ea3
Sho Fujita <fujisho@gmail.com>
2009-01-09 19:18:11 +0900

    supported frame filter in the kernel. fixed a bug related to nfds of select(). added FD_ZERO() before select().

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ CFLAGS=-g
 meshdump: main.o
 	$(CC) -o meshdump main.o -lpcap
 clean:
-	$(RM) meshdump *.o *~
+	$(RM) meshdump *.o *.dump *~
--- main.c
+++ main.c
@@ -29,12 +29,27 @@ void free_dumps(struct meshdump *dumps)
   free(dumps);
 }
 
+struct bpf_insn filter_insns[] = {
+  { 0x28, 0, 0, 0x00000002 },
+  { 0x15, 0, 1, 0x00001700 },
+  { 0x6, 0, 0, 0x00000057 },
+  { 0x6, 0, 0, 0x0000005a },
+};
+
 int open_dumps(struct meshdump *dumps, int dump_nr, char **ifnames)
 {
-  int i, err;
+  int i;
   char filename[128];
   char errbuf[PCAP_ERRBUF_SIZE];
+  struct bpf_program filter;
 
+  /* setup filter program */
+  memset(&filter, 0, sizeof(filter));
+  filter.bf_len = sizeof(filter_insns)/sizeof(filter_insns[0]);
+  filter.bf_insns = (struct bpf_insn*)malloc(sizeof(filter_insns));
+  memcpy(filter.bf_insns, &filter_insns, sizeof(filter_insns));
+
+  /* loop to initialize dumps */
   for (i = 0; i < dump_nr; ++i) {
 
     dumps[i].ifname = ifnames[i];
@@ -43,13 +58,19 @@ int open_dumps(struct meshdump *dumps, int dump_nr, char **ifnames)
                                     1,
                                     0,
                                     errbuf);
-    /* pcap_dispatch() is used later */
-    err = pcap_setnonblock(dumps[i].pcap, 1, errbuf);
-    if (err) {
+    if (dumps[i].pcap == 0) {
       fprintf(stderr, "%s\n", errbuf);
       return -1;
     }
-    if (dumps[i].pcap == 0) {
+    
+    /* this is a kind of optimization.. */
+    if (pcap_setfilter(dumps[i].pcap, &filter)) {
+      perror("pcap_setfilter");
+      return -1;
+    }
+      
+    /* pcap_dispatch() is used later */
+    if (pcap_setnonblock(dumps[i].pcap, 1, errbuf)) {
       fprintf(stderr, "%s\n", errbuf);
       return -1;
     }
@@ -66,6 +87,14 @@ int open_dumps(struct meshdump *dumps, int dump_nr, char **ifnames)
   return 0;
 }
 
+void print_stats(struct meshdump *dump)
+{
+  struct pcap_stat ps;
+  pcap_stats(dump->pcap, &ps);
+
+  printf("%s: received %d, dropped %d\n", dump->ifname, ps.ps_recv, ps.ps_drop);
+}
+
 void close_dumps(struct meshdump *dumps, int dump_nr)
 {
   int i;
@@ -76,8 +105,10 @@ void close_dumps(struct meshdump *dumps, int dump_nr)
       pcap_dump_close(dumps[i].dumper);
     }
     /* close pcap */
-    if (dumps[i].pcap)
+    if (dumps[i].pcap) {
+      print_stats(&dumps[i]);
       pcap_close(dumps[i].pcap);
+    }
   }
 }
 
@@ -89,16 +120,20 @@ void process_dumps(struct meshdump *dumps, int dump_nr)
   
   for (i = 0; i < dump_nr; ++i) {
     fds[i] = pcap_fileno(dumps[i].pcap);
-    if (fds[i] - 1 > nfds)
-      nfds = fds[i] - 1;
+    if (fds[i] + 1 > nfds)
+      nfds = fds[i] + 1;
   }
   
   while (running) {
     fd_set readfds;
     int err;
+
+    /* initialization */
+    FD_ZERO(&readfds);
     for (i = 0; i < dump_nr; ++i) {
       FD_SET(fds[i], &readfds);
     }
+    
     err = select(nfds, &readfds, NULL, NULL, NULL);
     if (err < 0)
       break;
@@ -111,7 +146,7 @@ void process_dumps(struct meshdump *dumps, int dump_nr)
                             pcap_dump,
                             (u_char*)dumps[i].dumper);
         if (err < 0) 
-          break;
+          continue;
               
       }
     }
